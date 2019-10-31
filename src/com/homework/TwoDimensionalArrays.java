5cb8b5bd8df808dff0b0bf0510ebe3dc7d5a773a
Astafeva Irina <astafeya99@gmail.com>
2019-10-31 15:33:41 +0300

    add outputMatrix

--- src/com/homework/TwoDimensionalArrays.java
+++ src/com/homework/TwoDimensionalArrays.java
@@ -22,4 +22,13 @@ public class TwoDimensionalArrays {
 
         return matrix;
     }
+
+    public static void outputMatrix(int[][] matrix, int rows, int columns) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
