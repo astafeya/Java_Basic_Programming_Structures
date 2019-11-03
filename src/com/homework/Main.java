1371822974b0cf6f9d4e4b7ede7911bc6e71ada8
Astafeva Irina <astafeya99@gmail.com>
2019-11-03 21:53:40 +0300

    add methods in OneDimensionalArrays class

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -17,6 +17,47 @@ public class Main {
         Drawing.drawTriangle(8, "bottom-left");
         Drawing.drawTriangle(8, "upper-right");
         Drawing.drawTriangle(8, "bottom-right");
-        
+
+        System.out.println("\nClass OneDimensionalArrays");
+        System.out.println("Task a");
+        int[] array1 = OneDimensionalArrays.createOddNumbersArray(1, 99);
+        OneDimensionalArrays.outputArray(array1);
+        Sorting.selectionSortingDescending(array1);
+        OneDimensionalArrays.outputArray(array1);
+
+        System.out.println("\nTask b");
+        int[] array2 = OneDimensionalArrays.createRandomArray(20, 0, 10);
+        OneDimensionalArrays.outputArray(array2);
+        System.out.println(OneDimensionalArrays.countOddAndEvenElements(array2));
+
+        System.out.println("\nTask c");
+        int[] array3 = OneDimensionalArrays.createRandomArray(10, 1, 100);
+        OneDimensionalArrays.outputArray(array3);
+        OneDimensionalArrays.replaceOddIndexes(array3, 0);
+        OneDimensionalArrays.outputArray(array3);
+
+        System.out.println("\nTask d");
+        int[] array4 = OneDimensionalArrays.createRandomArray(15, -50, 50);
+        System.out.println(OneDimensionalArrays.findMaxAndMin(array4));
+
+        System.out.println("\nTask e");
+        int[] array5 = OneDimensionalArrays.createRandomArray(10, 0, 10);
+        int[] array6 = OneDimensionalArrays.createRandomArray(10, 0, 10);
+        OneDimensionalArrays.outputArray(array5);
+        OneDimensionalArrays.outputArray(array6);
+        System.out.println("Avarage of array 1 = " + OneDimensionalArrays.findAvarage(array5));
+        System.out.println("Avarage of array 2 = " + OneDimensionalArrays.findAvarage(array6));
+        System.out.println(OneDimensionalArrays.compareAvarage(array5, array6));
+
+        System.out.println("\nTask f");
+        int[] array7 = OneDimensionalArrays.createRandomArray(20, -1, 1);
+        OneDimensionalArrays.outputArray(array7);
+        OneDimensionalArrays.outputArray(OneDimensionalArrays.findMostCommonElement(array7));
+
+        /*System.out.println("\nClass TwoDimensionalArrays");
+        System.out.println("Task a\n");
+        System.out.println("\nTask b\n");
+        System.out.println("\nTask c\n");
+        System.out.println("\nTask d\n");*/
     }
 }
--- src/com/homework/OneDimensionalArrays.java
+++ src/com/homework/OneDimensionalArrays.java
@@ -4,13 +4,12 @@ public class OneDimensionalArrays {
     public static int[] createRandomArray(int size, int minValue, int maxValue) {
         if (size <= 0) size = 10;
         int[] array = new int[size];
-        int max = maxValue - minValue + 1;
+        int max = maxValue - minValue;
         if (minValue == 0) {
             maxValue = 0;
         }
         else if (minValue > 0) {
             maxValue = -minValue;
-            max--;
         }
 
         for (int i = 0; i < size; i++) {
@@ -25,4 +24,114 @@ public class OneDimensionalArrays {
         }
         System.out.print("\n");
     }
+
+    public static int[] createOddNumbersArray(int startValue, int finishValue) {
+        int size = (finishValue + 1) / 2 - (startValue - 1) / 2;
+        int[] array = new int[size];
+
+        for (int i = 0, v = startValue; i < array.length; i++, v+=2) {
+            array[i] = v;
+        }
+
+        return array;
+    }
+
+    public static String countOddAndEvenElements(int[] array) {
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                even++;
+            }
+            else {
+                odd++;
+            }
+        }
+        
+        return "Odd elements: " + odd + "\nEven elements: " + even;
+    }
+
+    public static int findMaxIndex(int[] array) {
+        int maxIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[maxIndex] <= array[i]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    public static int findMinIndex(int[] array) {
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[minIndex] >= array[i]) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static String findMaxAndMin(int[] array) {
+        int maxIndex = findMaxIndex(array);
+        int minIndex = findMinIndex(array);
+
+        return "MaxElement: " + array[maxIndex] + "\nMaxIndex: " + maxIndex +
+                "\nMinElement: " + array[minIndex] + "\nMinIndex: " + minIndex;
+    }
+
+    public static double findAvarage(int[] array) {
+        double avarage = 0.0;
+        for (int i = 0; i < array.length; i++) {
+            avarage += array[i];
+        }
+        avarage /= array.length;
+        return avarage;
+    }
+
+    public static String compareAvarage(int[] array1, int[] array2) {
+        double avarage1 = findAvarage(array1);
+        double avarage2 = findAvarage(array2);
+        if (avarage1 == avarage2)
+            return "Avarages is equal";
+        return (avarage1 > avarage2)? "Avarage of array 1 is more" : "Avarage of array 2 is more";
+
+    }
+
+    public static int[] findMostCommonElement(int[] array) {
+        int[] answerElements = new int[array.length];
+        int answerLenght = 0;
+        int maxCount = 1;
+
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = i; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                    if (count == maxCount) {
+                        answerElements[answerLenght] = array[i];
+                        answerLenght++;
+                    }
+                    else if (count > maxCount) {
+                        maxCount = count;
+                        answerLenght = 1;
+                        answerElements[0] = array[i];
+                    }
+                }
+            }
+        }
+
+        int[] answer = new int[answerLenght];
+        for (int i = 0; i < answerLenght; i++) {
+            answer[i] = answerElements[i];
+        }
+        return answer;
+    }
+
+    public static void replaceOddIndexes(int[] array, int value) {
+        for (int i = 1; i < array.length; i+= 2) {
+            array[i] = value;
+        }
+    }
+
 }
