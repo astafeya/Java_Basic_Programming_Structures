0b7fd0982fae4d2f4f31aff61abebb2c0e6109a4
Astafeva Irina <astafeya99@gmail.com>
2019-11-03 23:23:47 +0300

    add TwoDimensionalArrays class

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -54,10 +54,30 @@ public class Main {
         OneDimensionalArrays.outputArray(array7);
         OneDimensionalArrays.outputArray(OneDimensionalArrays.findMostCommonElement(array7));
 
-        /*System.out.println("\nClass TwoDimensionalArrays");
-        System.out.println("Task a\n");
-        System.out.println("\nTask b\n");
-        System.out.println("\nTask c\n");
-        System.out.println("\nTask d\n");*/
+        System.out.println("\nClass TwoDimensionalArrays");
+        System.out.println("Task a");
+        int[][] matrix1 = TwoDimensionalArrays.createRandomMatrix(8, 8, 1, 99);
+        TwoDimensionalArrays.outputMatrix(matrix1, 8, 8);
+        System.out.println(TwoDimensionalArrays.sumMainDiagonalElements(matrix1, 8, 8) + "\n" +
+                TwoDimensionalArrays.multiplyMainDiagonalElements(matrix1, 8, 8) + "\n" +
+                TwoDimensionalArrays.sumSideDiagonalElements(matrix1, 8, 8) + "\n" +
+                TwoDimensionalArrays.multiplySideDiagonalElements(matrix1, 8, 8));
+
+        System.out.println("\nTask b");
+        int[][] matrix2 = TwoDimensionalArrays.createRandomMatrix(8, 5, -99, 99);
+        TwoDimensionalArrays.outputMatrix(matrix2, 8, 5);
+        System.out.println(TwoDimensionalArrays.maxElementInMatrix(matrix2, 8, 5));
+
+        System.out.println("\nTask c");
+        int[][] matrix3 = TwoDimensionalArrays.createRandomMatrix(8, 5, -10, 10);
+        TwoDimensionalArrays.outputMatrix(matrix3, 8, 5);
+        System.out.println(TwoDimensionalArrays.rowWithMaxElementsAbsMultiply(matrix3, 8, 5));
+
+        System.out.println("\nTask d");
+        int[][] matrix4 = TwoDimensionalArrays.createRandomMatrix(10, 7, 0, 100);
+        TwoDimensionalArrays.outputMatrix(matrix4, 10, 7);
+        TwoDimensionalArrays.sortRowsDescending(matrix4, 10, 7);
+        System.out.print("\n");
+        TwoDimensionalArrays.outputMatrix(matrix4, 10, 7);
     }
 }
--- src/com/homework/TwoDimensionalArrays.java
+++ src/com/homework/TwoDimensionalArrays.java
@@ -1,17 +1,18 @@
 package com.homework;
 
+import java.lang.reflect.Array;
+
 public class TwoDimensionalArrays {
     public static int[][] createRandomMatrix(int rows, int columns, int minValue, int maxValue) {
         if (rows <= 0) rows = 10;
         if (columns <= 0) columns = 10;
         int [][] matrix = new int[rows][columns];
-        int max = maxValue - minValue + 1;
+        int max = maxValue - minValue;
         if (minValue == 0) {
             maxValue = 0;
         }
         else if (minValue > 0) {
             maxValue = -minValue;
-            max--;
         }
 
         for (int i = 0; i < rows; i++) {
@@ -29,6 +30,97 @@ public class TwoDimensionalArrays {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.print("\n");
+        }
+    }
+
+    public static long sumMainDiagonalElements(int[][] matrix, int rows, int columns) {
+        long result = 0;
+        int minIndex = (rows > columns)? columns : rows;
+        for (int i = 0; i < minIndex; i++) {
+            result += matrix[i][i];
+        }
+        return result;
+    }
+
+    public static long multiplyMainDiagonalElements(int[][] matrix, int rows, int columns) {
+        long result = 1;
+        int minIndex = (rows > columns)? columns : rows;
+        for (int i = 0; i < minIndex; i++) {
+            result *= matrix[i][i];
+        }
+        return result;
+    }
+
+    public static long sumSideDiagonalElements(int[][] matrix, int rows, int columns) {
+        long result = 0;
+        int minIndex = (rows > columns)? columns : rows;
+        for (int i = 0; i < minIndex; i++) {
+            result += matrix[i][columns - 1 - i];
+        }
+        return result;
+    }
+
+    public static long multiplySideDiagonalElements(int[][] matrix, int rows, int columns) {
+        long result = 1;
+        int minIndex = (rows > columns)? columns : rows;
+        for (int i = 0; i < minIndex; i++) {
+            result *= matrix[i][columns - 1 - i];
+        }
+        return result;
+    }
+
+    public static String maxElementInMatrix(int[][] matrix, int rows, int columns) {
+        int max = matrix[0][0];
+        int maxRow = 0;
+        int maxColumn = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                    maxRow = i;
+                    maxColumn = j;
+                }
+            }
+        }
+        return "Max Element = " + max + "\nIndex: [" + maxRow + "][" + maxColumn + "]";
+    }
+
+    public static int rowWithMaxElementsAbsMultiply(int[][] matrix, int rows, int columns) {
+        long[] multiplies = new long[rows];
+        for (int i = 0; i < rows; i++) {
+            long rowMultiply = 1;
+            for (int j = 0; j < columns; j++){
+                rowMultiply *= matrix[i][j];
+            }
+            multiplies[i] = Math.abs(rowMultiply);
+        }
+        int maxRow = 0;
+        long max = multiplies[0];
+        for (int i = 1; i < columns; i++) {
+            if (multiplies[i] > max) {
+                max = multiplies[i];
+                maxRow = i;
+            }
+        }
+        return maxRow;
+    }
+
+    public static void sortRowsDescending(int[][] matrix, int rows, int columns) {
+        for (int i = 0; i < rows; i++) {
+            int tmp;
+            for (int j = 0; j < columns - 1; j++) {
+                int max = j;
+                for (int k = j + 1; k < columns; k++) {
+                    if (matrix[i][k] > matrix[i][max]) {
+                        max = k;
+                    }
+                }
+                tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][max];
+                matrix[i][max] = tmp;
+            }
         }
     }
 }
